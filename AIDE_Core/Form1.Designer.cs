abefbbf8f6c0ce0f174f003da8027cfbd6f8a45f
Daniel Ramirez <DanielRamirezDiaz@outlook.com>
2019-05-24 10:13:09 -0700

    Initial Commit

---
+++ AIDE_Core/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace AIDE_Core
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBoxConnection = new System.Windows.Forms.GroupBox();
+            this.buttonDisconnect = new System.Windows.Forms.Button();
+            this.comboBoxCameras = new System.Windows.Forms.ComboBox();
+            this.videoSourcePlayerFiltered = new AForge.Controls.VideoSourcePlayer();
+            this.videoSourcePlayerClone = new AForge.Controls.VideoSourcePlayer();
+            this.videoSourcePlayerOriginal = new AForge.Controls.VideoSourcePlayer();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.groupBoxConnection.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBoxConnection
+            // 
+            this.groupBoxConnection.Controls.Add(this.buttonDisconnect);
+            this.groupBoxConnection.Controls.Add(this.comboBoxCameras);
+            this.groupBoxConnection.Location = new System.Drawing.Point(13, 403);
+            this.groupBoxConnection.Name = "groupBoxConnection";
+            this.groupBoxConnection.Size = new System.Drawing.Size(190, 100);
+            this.groupBoxConnection.TabIndex = 17;
+            this.groupBoxConnection.TabStop = false;
+            this.groupBoxConnection.Text = "Connection";
+            // 
+            // buttonDisconnect
+            // 
+            this.buttonDisconnect.Location = new System.Drawing.Point(110, 53);
+            this.buttonDisconnect.Name = "buttonDisconnect";
+            this.buttonDisconnect.Size = new System.Drawing.Size(75, 23);
+            this.buttonDisconnect.TabIndex = 10;
+            this.buttonDisconnect.Text = "Disconnect";
+            this.buttonDisconnect.UseVisualStyleBackColor = true;
+            this.buttonDisconnect.Click += new System.EventHandler(this.ButtonDisconnect_Click);
+            // 
+            // comboBoxCameras
+            // 
+            this.comboBoxCameras.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxCameras.FormattingEnabled = true;
+            this.comboBoxCameras.Location = new System.Drawing.Point(17, 26);
+            this.comboBoxCameras.Name = "comboBoxCameras";
+            this.comboBoxCameras.Size = new System.Drawing.Size(167, 21);
+            this.comboBoxCameras.TabIndex = 5;
+            this.comboBoxCameras.Visible = false;
+            // 
+            // videoSourcePlayerFiltered
+            // 
+            this.videoSourcePlayerFiltered.Location = new System.Drawing.Point(808, 12);
+            this.videoSourcePlayerFiltered.Name = "videoSourcePlayerFiltered";
+            this.videoSourcePlayerFiltered.Size = new System.Drawing.Size(392, 385);
+            this.videoSourcePlayerFiltered.TabIndex = 16;
+            this.videoSourcePlayerFiltered.Text = "videoSourcePlayer3";
+            this.videoSourcePlayerFiltered.VideoSource = null;
+            this.videoSourcePlayerFiltered.NewFrame += new AForge.Controls.VideoSourcePlayer.NewFrameHandler(this.videoSourcePlayerFiltered_NewFrame);
+            // 
+            // videoSourcePlayerClone
+            // 
+            this.videoSourcePlayerClone.Location = new System.Drawing.Point(410, 12);
+            this.videoSourcePlayerClone.Name = "videoSourcePlayerClone";
+            this.videoSourcePlayerClone.Size = new System.Drawing.Size(392, 385);
+            this.videoSourcePlayerClone.TabIndex = 15;
+            this.videoSourcePlayerClone.Text = "videoSourcePlayer2";
+            this.videoSourcePlayerClone.VideoSource = null;
+            this.videoSourcePlayerClone.NewFrame += new AForge.Controls.VideoSourcePlayer.NewFrameHandler(this.videoSourcePlayerClone_NewFrame);
+            // 
+            // videoSourcePlayerOriginal
+            // 
+            this.videoSourcePlayerOriginal.Location = new System.Drawing.Point(12, 12);
+            this.videoSourcePlayerOriginal.Name = "videoSourcePlayerOriginal";
+            this.videoSourcePlayerOriginal.Size = new System.Drawing.Size(392, 385);
+            this.videoSourcePlayerOriginal.TabIndex = 14;
+            this.videoSourcePlayerOriginal.Text = "videoSourcePlayer1";
+            this.videoSourcePlayerOriginal.VideoSource = null;
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(13, 509);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(75, 23);
+            this.buttonStart.TabIndex = 13;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.ButtonStart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1227, 556);
+            this.Controls.Add(this.groupBoxConnection);
+            this.Controls.Add(this.videoSourcePlayerFiltered);
+            this.Controls.Add(this.videoSourcePlayerClone);
+            this.Controls.Add(this.videoSourcePlayerOriginal);
+            this.Controls.Add(this.buttonStart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Deactivate += new System.EventHandler(this.ButtonDisconnect_Click);
+            this.groupBoxConnection.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBoxConnection;
+        private System.Windows.Forms.Button buttonDisconnect;
+        private System.Windows.Forms.ComboBox comboBoxCameras;
+        private AForge.Controls.VideoSourcePlayer videoSourcePlayerFiltered;
+        private AForge.Controls.VideoSourcePlayer videoSourcePlayerClone;
+        private AForge.Controls.VideoSourcePlayer videoSourcePlayerOriginal;
+        private System.Windows.Forms.Button buttonStart;
+    }
+}
+
---
+++ AIDE_Core/Form1.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using AForge.Imaging;
+using AForge.Imaging.Filters;
+using AForge;
+using AForge.Video.DirectShow;
+using System.Drawing.Imaging;
+using System.Threading;
+using System.IO;
+
+namespace AIDE_Core
+{
+    public partial class Form1 : Form
+    {
+        private FilterInfoCollection videoDevices;
+        EuclideanColorFiltering filter = new EuclideanColorFiltering();
+        Color color = Color.Black;
+        GrayscaleBT709 grayscaleFilter = new GrayscaleBT709();
+        BlobCounter blobCounter = new BlobCounter();
+        int range = 120;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            blobCounter.MinWidth = 2;
+            blobCounter.MinHeight = 2;
+            blobCounter.FilterBlobs = true;
+            blobCounter.ObjectsOrder = ObjectsOrder.Size;
+            try
+            {
+                // enumerate video devices
+                videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+
+                if (videoDevices.Count == 0)
+                    throw new ApplicationException();
+
+                // add all devices to combo
+                foreach (FilterInfo device in videoDevices)
+                {
+                    comboBoxCameras.Items.Add(device.Name);
+                }
+
+                comboBoxCameras.SelectedIndex = 0;
+            }
+            catch (ApplicationException)
+            {
+                comboBoxCameras.Items.Add("No local capture devices");
+                videoDevices = null;
+            }
+
+            Bitmap b = new Bitmap(320, 240);
+            // Rectangle a = (Rectangle)r;
+            Pen pen1 = new Pen(Color.FromArgb(160, 255, 160), 3);
+            Graphics g2 = Graphics.FromImage(b);
+            pen1 = new Pen(Color.FromArgb(255, 0, 0), 3);
+            g2.Clear(Color.White);
+            g2.DrawLine(pen1, b.Width / 2, 0, b.Width / 2, b.Width);
+            g2.DrawLine(pen1, b.Width, b.Height / 2, 0, b.Height / 2);
+        }
+
+        private void videoSourcePlayerClone_NewFrame(object sender, ref Bitmap image)
+        {
+            Bitmap objectsImage = null;
+            Bitmap mImage = null;
+            mImage = (Bitmap)image.Clone();
+            filter.CenterColor = new RGB(color);
+            filter.Radius = (short)range;
+
+            objectsImage = image;
+            filter.ApplyInPlace(objectsImage);
+
+            BitmapData objectsData = objectsImage.LockBits(new Rectangle(0, 0, image.Width, image.Height),
+            ImageLockMode.ReadOnly, image.PixelFormat);
+            UnmanagedImage grayImage = grayscaleFilter.Apply(new UnmanagedImage(objectsData));
+            objectsImage.UnlockBits(objectsData);
+
+
+            blobCounter.ProcessImage(grayImage);//get rectangles
+            Rectangle[] rects = blobCounter.GetObjectsRectangles();//store rects in a varibla
+
+            if (rects.Length > 0)//draw blob rectangles on display
+            {
+
+                foreach (Rectangle objectRect in rects)
+                {
+                    Graphics g = Graphics.FromImage(mImage);
+                    using (Pen pen = new Pen(Color.FromArgb(160, 255, 160), 5))
+                    {
+                        g.DrawRectangle(pen, objectRect);
+                    }
+
+                    g.Dispose();
+                }
+            }
+
+            image = mImage;
+        }
+
+        public void videoSourcePlayerFiltered_NewFrame(object sender, ref Bitmap image)
+        {
+            Bitmap objectsImage = null;
+
+
+            // set center colol and radius
+            filter.CenterColor = new RGB(color);//Color.FromArgb(color.ToArgb());
+            filter.Radius = (short)range;
+            // apply the filter
+            objectsImage = image;
+            filter.ApplyInPlace(image);
+
+            // lock image for further processing
+            BitmapData objectsData = objectsImage.LockBits(new Rectangle(0, 0, image.Width, image.Height),
+                ImageLockMode.ReadOnly, image.PixelFormat);
+
+            // grayscaling
+            UnmanagedImage grayImage = grayscaleFilter.Apply(new UnmanagedImage(objectsData));
+
+            // unlock image
+            objectsImage.UnlockBits(objectsData);
+
+            // locate blobs 
+            blobCounter.ProcessImage(grayImage);
+            Rectangle[] rects = blobCounter.GetObjectsRectangles();
+
+            if (rects.Length > 0)
+            {
+                Rectangle objectRect = rects[0];
+
+                // draw rectangle around detected object
+                Graphics g = Graphics.FromImage(image);
+
+                using (Pen pen = new Pen(Color.FromArgb(160, 255, 160), 5))
+                {
+                    g.DrawRectangle(pen, objectRect);
+                }
+                g.Dispose();
+            }
+        }
+
+        private void ButtonStart_Click(object sender, EventArgs e)
+        {
+            videoSourcePlayerOriginal.SignalToStop();
+            videoSourcePlayerOriginal.WaitForStop();
+            videoSourcePlayerClone.SignalToStop();
+            videoSourcePlayerClone.WaitForStop();
+            videoSourcePlayerFiltered.SignalToStop();
+            videoSourcePlayerFiltered.WaitForStop();
+            // videoDevices = null;
+            VideoCaptureDevice videoSource = new VideoCaptureDevice(videoDevices[comboBoxCameras.SelectedIndex].MonikerString);
+            videoSource.DesiredFrameSize = new Size(320, 240);
+            videoSource.DesiredFrameRate = 12;
+
+            videoSourcePlayerOriginal.VideoSource = videoSource;
+            videoSourcePlayerOriginal.Start();
+            videoSourcePlayerClone.VideoSource = videoSource;
+            videoSourcePlayerClone.Start();
+            videoSourcePlayerFiltered.VideoSource = videoSource;
+            videoSourcePlayerFiltered.Start();
+            //groupBox1.Enabled = false;
+        }
+
+        private void ButtonDisconnect_Click(object sender, EventArgs e)
+        {
+            videoSourcePlayerOriginal.SignalToStop();
+            videoSourcePlayerOriginal.WaitForStop();
+            videoSourcePlayerClone.SignalToStop();
+            videoSourcePlayerClone.WaitForStop();
+            videoSourcePlayerFiltered.SignalToStop();
+            videoSourcePlayerFiltered.WaitForStop();
+        }
+    }
+}
